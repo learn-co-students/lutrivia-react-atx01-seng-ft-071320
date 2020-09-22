ddecdb783633bb1ad7b45257ca7f9a74a9c1d3d1
Ceddyp19 <ceddyp117@gmail.com>
2020-09-22 17:11:34 -0500

    Done.

---
+++ lutrivia/src/Footer.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+class Footer extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <input type='text' />
+                <button> Submit score </button>
+            </div>
+        );
+    }
+}
+
+export default Footer;
